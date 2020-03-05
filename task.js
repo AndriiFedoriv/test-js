3f6c05dae7f1490073ab48634ef08b4f8cd285f9
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2020-03-05 13:04:18 +0200

    Create task.js

---
+++ task.js
@@ -0,0 +1,5 @@
+//  https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript?fbclid=IwAR3wUSZtJCH9klgGdl1LbeKGPF8WQ-JH3xfVLcjc4ivDaYjitmkUbao4xP8
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  const result = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return result;// code goes here
+}
