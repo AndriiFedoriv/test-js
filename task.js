30f47eba73b398846f1c9e9d5295d26c73482fad
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2020-03-05 15:26:01 +0200

    Update task.js

--- task.js
+++ task.js
@@ -3,3 +3,11 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   const result = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
     return result; // code goes here
 }
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript?fbclid=IwAR2Kvbg02f_8BwB3yaqopCid0l3wSad7gGfnGXltMOXWOEPusm890YxT4V4
+function greet (name, owner) {
+if (name === owner) {
+return "Hello boss";
+}
+return "Hello guest"; // Add code here
+}
